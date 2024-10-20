9d47ae95cb5e4847c507d5742ff3d0dfe95352c9
BRYNS-BATTLESHI\bryna <brynaytes@gmail.com>
2024-10-20 13:44:34 -0500

    Working local server for backend

--- BackEndMocking/app.js
+++ BackEndMocking/app.js
@@ -1,15 +1,19 @@
 import { handler } from './src/index.mjs' 
+import express from 'express';
 
-const express = require('express')
+//const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/meeting', (req, res) => {
-    res.send( handler(req));
+ app.post('/meetings', (req, res) => {
+    //The 'then' portion here waits for the async response to resolve
+    handler(req.body).then((temp) => res.send(temp ))
   })
 
 app.listen(port, () => {
